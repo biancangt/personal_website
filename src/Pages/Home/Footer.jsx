e1dd97afb03e85cfd4df501ff46aed60ff8f97a4
Bianca Nagata <biancanagata@Biancas-MacBook-Air.local>
2025-05-21 14:49:34 -0500

    portfolio links added

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer--container">
-      <hr className="divider" />
       <div className="footer--content--container">
         <p className="footer--content">Made by Bianca Nagata</p>
       </div>
